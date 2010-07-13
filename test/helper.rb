2619db563789c2b3b9d30dbe48422d67033c3094
Martin Karlsch <martin@karlsch.com>
2010-07-13 11:22:58 +0200

    added a small description

--- test/helper.rb
+++ test/helper.rb
@@ -1,10 +1,10 @@
 require 'rubygems'
 require 'test/unit'
-require 'shoulda'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'fastgeoip'
 
 class Test::Unit::TestCase
+  # TODO needs testcases
 end
